9e330af51c90bbd1c223374a22eee45fe910ea41
noderoid64 <noderoid64@gmail.com>
2021-02-05 11:57:32 +0200

    (chor) add daos

---
+++ Presistance/DAO/AdvertisementDAO.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertisingApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvertisingApi.Presistance.DAO
+{
+    public class AdvertisementDAO
+    {
+        private AppDbContext _appDbContext;
+
+        public AdvertisementDAO(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Task SaveAdvertisementAsync(Advertisement add)
+        {
+            _appDbContext.Add(add);
+            return _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Advertisement> GetNextAddAsync()
+        {
+            long i = _appDbContext.GetNextQueueSequenceValue();
+            Advertisement add = _appDbContext.Advertisements
+                .Include(ad => ad.Category)
+                .FirstOrDefault(ad => ad.AdvertisementId >= i);
+            if (add == null)
+            {
+                _appDbContext.ResetQueueSequence();
+                i = 1;
+                add = _appDbContext.Advertisements
+                    .Include(ad => ad.Category)
+                    .FirstOrDefault(ad => ad.AdvertisementId >= i);
+            }
+            else if (add.AdvertisementId != i)
+            {
+                _appDbContext.SetQueueSequenceValue(add.AdvertisementId + 1);
+            }
+            
+            if (add != null)
+                add.Views++;
+            
+            await _appDbContext.SaveChangesAsync();
+
+            return add;
+        }
+
+        public async Task<Advertisement> GetRelevantAsync(AdType type, string category, ICollection<string> tags)
+        {
+            var query = from s in _appDbContext.Advertisements
+                    .Include(ad => ad.Category)
+                where
+                    s.AdType == type &&
+                    s.Category.Title == category &&
+                    (
+                        !tags.Any() ||
+                        s.Tags.Any(t => tags.Contains(t.Title))
+                    )
+                select s;
+
+            Advertisement result = query.FirstOrDefault();
+            
+            if (result != null)
+            {
+                result.Views++;
+                await _appDbContext.SaveChangesAsync();
+            }
+
+            return result;
+        }
+
+        public async Task DeleteAsync(long id)
+        {
+            var obj = await _appDbContext.Advertisements.FirstOrDefaultAsync(ad => ad.AdvertisementId.Equals(id));
+            if (obj != null)
+            {
+                _appDbContext.Advertisements.Remove(obj);
+                await _appDbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new Exception($"Object with id: {id} doesn't exist");
+            }
+        }
+    }
+}
---
+++ Presistance/DAO/StatisticDAO.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertisingApi.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace AdvertisingApi.Presistance.DAO
+{
+    public class StatisticDAO
+    {
+        private AppDbContext _appDbContext;
+
+        public StatisticDAO(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<dynamic> getAllViewsByTypeAsync()
+        {
+
+            return await _appDbContext.Advertisements
+                .GroupBy(ad => ad.AdType)
+                .Select(g => new {AdType = g.Key, Views = g.Sum(m => m.Views)})
+                .ToListAsync();
+
+        }
+
+        public dynamic getTop3Categories()
+        {
+            return _appDbContext.Advertisements
+                .Include(ad => ad.Category)
+                .AsEnumerable()
+                .GroupBy(ad => ad.Category.Title)
+                .Select(g => new {Category = g.Key, PostCount = g.Count()})
+                .OrderBy(g => g.PostCount)
+                .Take(3)
+                .ToList();
+        }
+
+        public async Task<dynamic> getTop10PostsByViews()
+        {
+            return await _appDbContext.Advertisements
+                .OrderBy(ad => ad.Views)
+                .Take(10)
+                .Select(ad => new {Post = ad.Content, Views = ad.Views})
+                .ToListAsync();
+        }
+        
+        public async Task<dynamic> getTop15TagsByViews()
+        {
+            return await _appDbContext.Tags
+                .Include(t => t.Advertisements)
+                .OrderBy(t => t.Advertisements.Sum(ad => ad.Views))
+                .Take(15)
+                .Select(t => new {Title = t.Title, Views = t.Advertisements.Sum(ad => ad.Views)})
+                .ToListAsync();
+        }
+    }
+}
